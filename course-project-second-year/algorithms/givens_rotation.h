87d3cd5ed036ccfe92352325ec6df3d4cc1b45a0
polipolinom <polinasha960@gmail.com>
2024-02-13 13:33:08 +0300

    fixed ginves

--- course-project-second-year/algorithms/givens_rotation.h
+++ course-project-second-year/algorithms/givens_rotation.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <utility>
 
-#include "../ types / matrix.h"
+#include "../types/matrix.h"
 
 namespace svd_computation {
 inline std::pair<long double, long double> get_givens_rotation(long double a, long double b,
