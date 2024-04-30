6f73a748b137d40853d452b6153c1f0514aef570
polipolinom <polinasha960@gmail.com>
2024-05-01 02:09:51 +0300

    add mult and transpose for band

--- course-project-second-year/types/matrix.h
+++ course-project-second-year/types/matrix.h
@@ -342,6 +342,36 @@ Matrix<Type> transpose(const Matrix<Type>& A) noexcept {
     return result;
 }
 
+template <typename Type>
+Matrix<Type> mult_band(const Matrix<Type>& A, const size_t A_low_band_width, const size_t A_upper_band_width,
+                       const Matrix<Type>& B, const size_t B_low_band_width, const size_t B_upper_band_width) noexcept {
+    assert(A.width() == B.height());
+    Matrix<Type> result(A.height(), B.width());
+    for (int i = 0; i < A.width(); ++i) {
+        for (int indA = std::max(0, i - (int)(A_low_band_width));
+             indA < std::min(A.height(), i + (int)(A_upper_band_width)); ++indA) {
+            for (int indB = std::max(0, indB - (int)(B_low_band_width));
+                 indB < std::min(B.width(), i + (int)(B_upper_band_width)); ++indB) {
+                result(indA, indB) += A(indA, i) * B(i, indB);
+            }
+        }
+    }
+    return result;
+}
+
+template <typename Type>
+Matrix<Type> transpose_band(const Matrix<Type>& A, const size_t low_band_width,
+                            const size_t upper_band_width) noexcept {
+    Matrix<Type> result(A.width(), A.height());
+    for (int i = 0; i < A.height(); ++i) {
+        for (int j = std::max(0, i - (int)(low_band_width)); j < std::min(A.width(), i + (int)(upper_band_width));
+             ++j) {
+            result(j, i) = A(i, j);
+        }
+    }
+    return result;
+}
+
 template <>
 inline Matrix<Complex>& Matrix<Complex>::conjugate() noexcept {
     transpose();
